b13e57ac964afd7c007a41a88db886feee64fea0
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2018-08-13 14:31:08 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "prglib.h"
+
+using namespace prglib;
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    cout << "Escreva um programa." << endl;
+    return 0;
+}
